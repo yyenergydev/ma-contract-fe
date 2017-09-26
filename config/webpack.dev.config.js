d9f524abe5098adf249ce190ccdf3a67f1a0da38
tanghea <31943535+tanghea@users.noreply.github.com>
2017-09-26 11:36:11 +0800

    Add files via upload

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -22,10 +22,11 @@ configs.forEach(function(config){
         changeOrigin: true,
         host: "yc.yonyou.com"
       },
-      '/file1': {
+      '/file': {
         target: 'http://yc.yonyou.com',
         secure: false,
         changeOrigin: true,
+        pathRewrite: {'^/file': '/file1'},
         host: "yc.yonyou.com"
       },
       '/mall-basedoc': {
@@ -34,14 +35,6 @@ configs.forEach(function(config){
         changeOrigin: true,
         host: "yc.yonyou.com"
       },
-      '/cpu-score': {
-        // target: 'http://172.20.4.39:9929',
-        target: 'http://10.1.80.51:9909',
-        secure: false,
-        changeOrigin: true,
-        host: "10.1.80.51"
-        //host: "10.1.80.38"
-      },
       '/cpu-project-def': {
         target: 'http://172.20.4.66:9922',
         // target:'http://10.1.80.32:8090',
@@ -49,59 +42,11 @@ configs.forEach(function(config){
         changeOrigin: true,
         host: "172.20.4.66"
       },
-      '/cpu-supplierbid': {
-        target: 'http://172.20.4.66:9924',
-        secure: false,
-        changeOrigin:true,
-        host: '172.20.4.66'
-      },
-      '/cpu-biddoc': {
-        target: 'http://172.20.4.66:9923',
-        //target: 'http://10.1.80.35:8090',
-        secure: false,
-        changeOrigin: true,
-        host: '172.20.4.66',
-        //host: '10.1.80.35',
-      },
       '/cpu-basedocrefer': {
         target: 'http://172.20.4.66:9925',
         secure: false,
         changeOrigin: true,
         host: '172.20.4.66'
-      },
-      '/cpu-pricedecision-web': {
-        target: 'http://172.20.4.67:9906',
-        secure: false,
-        changeOrigin: true,
-        host: '172.20.4.67'
-      },
-      '/cpu-bidopen': {
-        target: 'http://172.20.4.67:9927',
-        secure: false,
-        changeOrigin: true,
-        host: '172.20.4.67',
-      },
-      '/cpu-expert': {
-        target: 'http://172.20.4.38:9926',
-        secure: false,
-        changeOrigin: true,
-        host: '172.20.4.38',
-      },
-      '/contract': {
-        target: 'http://yc.yonyou.com',
-        //target: 'http://10.1.80.46:9999/',
-        secure: false,
-        changeOrigin: true,
-        //host: '10.1.80.46',
-        host: 'yc.yonyou.com',
-      },
-      '/cpu-bidnotice': {
-        target: 'http://yc.yonyou.com',
-        //    target: 'http://127.0.0.1:8090/',
-        secure: false,
-        changeOrigin: true,
-        //host: '10.1.80.24',
-        host: 'yc.yonyou.com'
       },
       '/cpu-bidtrade': {
         target: 'http://yc.yonyou.com',
@@ -109,7 +54,6 @@ configs.forEach(function(config){
         secure: false,
         changeOrigin: true,
         host: 'yc.yonyou.com'
-        // host: '10.6.253.33'
       },
       '/ma-contract': {
         target: 'http://10.6.194.119:9999',

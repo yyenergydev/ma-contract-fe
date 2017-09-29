7b209da0722cbc2794116f2b43f283d448f026fe
马红更 <mahga@yonyou.com>
2017-09-29 16:29:48 +0800

    合同类型参照修改

--- src/components/modalrefer/contracttype/index.js
+++ src/components/modalrefer/contracttype/index.js
@@ -1,5 +1,4 @@
-// latest: zhangmyh 2017-3-17 9:08 AM
-// 部门参照
+// latest: mahonggeng 2017-9-17 9:08 AM
 /* global $ u */
 import Collection from 'common/vo/collection'
 import 'collection/contracttype'
@@ -10,45 +9,18 @@ import { forEach } from 'lodash'
 function init (refer) {
   // 树形控件加载有误,所以需要每次都重新加载
   let trees = Collection.create('collection.contracttype')
-  let enterpriseId = refer.options.enterpriseId || window.global.user.enterpriseId
-  trees.load({ enterpriseId: enterpriseId })
+  trees.load()
   let selectItem = {}
   let vm = {
     treemodel: trees.datatable,
     treeOption: {
-      /* check: {
-        enable: true
-      }, */
       callback: {
-        /* onCheck: function () {
-          console.log('zmy', arguments)
-        }, */
         beforeClick: async function (id, obj) {
           if (!obj.flag) {
             let { result } = await Post('/ma-contract/contracttype/list', {
               //enterpriseId: enterpriseId,
               parentId: obj.id
             })
-           /* _config.hasDepartment = true
-            if (_config.hasDepartment) {
-              trees.datatable.addSimpleData(result)
-            } else {
-              let newdata = []
-              result.forEach(function (orginfo) {
-                console.log('yusn', orginfo)
-                if (parseInt(orginfo.orgType) >= 2) {
-                  newdata.push(orginfo)
-                }
-              })
-              trees.datatable.addSimpleData(newdata)
-            } */
-            /* let newdata = []
-            result.forEach(function (orginfo) {
-              //if (parseInt(orginfo.orgType) >= 2) {
-                newdata.push(orginfo)
-              //}
-            }) */
-            //trees.datatable.addSimpleData(result[0])
             obj.flag = true
           }
           selectItem = obj
@@ -64,18 +36,6 @@ function init (refer) {
   })
 }
 let template = require('./index.html')
-/* export default function (config) {
-  _config = Object.assign({
-    hasDepartment: true,
-    multiSelect: false
-  }, config)
-
-  return {
-    init,
-    template
-  }
-}
-*/
 export default {
   init,
   template
